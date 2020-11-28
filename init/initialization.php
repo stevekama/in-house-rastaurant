f3e48ceeb10a163d4f7109beaa1dcf613f3ac8b0
steve kama <stevekamahertz@gmail.com>
2020-11-28 03:37:45 +0300

    base url

--- init/initialization.php
+++ init/initialization.php
@@ -2,8 +2,8 @@
 defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
 // defined('SITE_ROOT') ? null : define('SITE_ROOT', DS.'app');
 // defined('SITE_ROOT') ? null : define('SITE_ROOT', DS.'home2'.DS.'tadteica'.DS.'public_html');
-// defined('SITE_ROOT') ? null : define('SITE_ROOT', DS.'app');
-defined('SITE_ROOT') ? null : define('SITE_ROOT', DS.'xampp'.DS.'htdocs'.DS.'in-house-restaurant');
+defined('SITE_ROOT') ? null : define('SITE_ROOT', DS.'app');
+// defined('SITE_ROOT') ? null : define('SITE_ROOT', DS.'xampp'.DS.'htdocs'.DS.'in-house-restaurant');
 defined('CONFIG_PATH') ? null : define('CONFIG_PATH', SITE_ROOT.DS.'config');
 defined('INIT_PATH') ? null : define('INIT_PATH', SITE_ROOT.DS.'init');
 defined('MODELS_PATH') ? null : define('MODELS_PATH', SITE_ROOT.DS.'models');
@@ -12,8 +12,8 @@ defined('PUBLIC_PATH') ? null : define('PUBLIC_PATH', SITE_ROOT.DS.'public');
 defined('MIGRATION_PATH') ? null : define('MIGRATION_PATH', SITE_ROOT.DS.'migrations');
 
 // $site_url = "https://tadtechafrica.com/";
-// $site_url = "https://tadtechafrica.herokuapp.com/";
-$site_url = "http://localhost/in-house-restaurant/";
+$site_url = "https://in-house-restaurant.herokuapp.com/";
+// $site_url = "http://localhost/in-house-restaurant/";
 
 // db connections
 require_once(CONFIG_PATH.DS.'database.php');
